5ed15a5cc7a9519ef29506f9db5a894243dabe66
Owen Oertell <ojo2@cornell.edu>
2026-03-18 20:18:26 -0400

    Adding

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CXxRtQ7r.js","_app/immutable/chunks/J2exRR6P.js","_app/immutable/chunks/BgBjWlh1.js","_app/immutable/chunks/DvSjV8Dt.js","_app/immutable/chunks/CLVaxqoL.js","_app/immutable/chunks/BnBwa5gP.js","_app/immutable/chunks/BFDdB9wi.js","_app/immutable/chunks/BA6G6jUo.js","_app/immutable/chunks/C00GmUgo.js","_app/immutable/chunks/BlpvKRpP.js","_app/immutable/chunks/Btlb4kqP.js","_app/immutable/chunks/DM_2-J8O.js"];
+export const imports = ["_app/immutable/nodes/1.89jsn5Gd.js","_app/immutable/chunks/J2exRR6P.js","_app/immutable/chunks/BgBjWlh1.js","_app/immutable/chunks/DvSjV8Dt.js","_app/immutable/chunks/CLVaxqoL.js","_app/immutable/chunks/BzaMCobw.js","_app/immutable/chunks/BjXCfHD9.js","_app/immutable/chunks/BA6G6jUo.js","_app/immutable/chunks/C00GmUgo.js","_app/immutable/chunks/BlpvKRpP.js","_app/immutable/chunks/Btlb4kqP.js","_app/immutable/chunks/DM_2-J8O.js"];
 export const stylesheets = [];
 export const fonts = [];
